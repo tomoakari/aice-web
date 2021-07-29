0f092b4004fad9e06ccd2180246743d073a6b6f7
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2021-07-29 16:52:26 +0900

    bf

--- index.js
+++ index.js
@@ -16,10 +16,15 @@ var options = {
 const express = require('express')
 const app = express()
 const server = require('https').createServer(options, app);
-const io = require('socket.io')(server)
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+    }
+});
 const { ExpressPeerServer } = require('peer');
 //const port = 3000
-
+const cors = require('cors')
+app.use(cors())
 
 
 const peerServer = ExpressPeerServer(server, {
@@ -32,7 +37,8 @@ app.use('/peerjs', peerServer)
 // EJS、ファイルアクセス
 app.set('view engine', 'ejs')
 app.set("views", __dirname + "/views");
-app.use(express.static('public'));
+app.set("public", __dirname + "/public");
+//app.use(express.static('public'));
 
 server.listen(port, () => {
     console.log('listening on *:' + port);
