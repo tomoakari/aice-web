380ab2191fb8764032152ce685a07931324c704f
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2021-07-29 16:30:00 +0900

    upload1

---
+++ index.js
@@ -0,0 +1,106 @@
+/**
+ ***************************************************************
+ * 設定
+ ***************************************************************
+ */
+var fs = require("fs");
+
+const port = 8446;
+const ssl_server_key = "/etc/letsencrypt/live/conftest.aice.cloud/privkey.pem";
+const ssl_server_crt = "/etc/letsencrypt/live/conftest.aice.cloud/fullchain.pem";
+var options = {
+    key: fs.readFileSync(ssl_server_key),
+    cert: fs.readFileSync(ssl_server_crt),
+};
+
+const express = require('express')
+const app = express()
+const server = require('https').createServer(options, app);
+const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer');
+//const port = 3000
+
+
+
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+})
+
+app.use('/peerjs', peerServer)
+
+
+// EJS、ファイルアクセス
+app.set('view engine', 'ejs')
+app.set("views", __dirname + "/views");
+app.use(express.static('public'));
+
+server.listen(port, () => {
+    console.log('listening on *:' + port);
+})
+
+/**
+ ***************************************************************
+ * ルーティング
+ ***************************************************************
+ */
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/aice', (req, res) => {
+    // res.render('view/client');
+    // res.sendFile(__dirname + "/views/index_aiforus.html");
+    res.render("./client.ejs");
+});
+
+
+
+/**
+ ***************************************************************
+ * シグナリング
+ ***************************************************************
+ */
+const rooms = []
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.emit('message', 'ログインしました');
+    socket.broadcast.emit('message', '新しいユーザが接続されました。');
+
+    socket.on('message', (msg) => {
+        const room = rooms.find(room => room.id == socket.id)
+        if (room) io.to(room.roomId).emit('message', `${room.name}: ${msg}`)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'ユーザからの接続が切れました。')
+    });
+
+
+    socket.on('join-room', (roomId, name, peerId) => {
+        rooms.push({
+            roomId,
+            name,
+            id: socket.id,
+            peerId
+        })
+        socket.join(roomId);
+        console.log("roomId:" + roomId)
+        socket.emit('message', `Bot: ${name}さん、Zoomクローンにようこそ!`);
+        socket.broadcast.to(roomId).emit('message', `Bot: ${name}さんが接続しました。`)
+        socket.broadcast.to(roomId).emit('user-connected', peerId, socket.id)
+        const members = rooms.filter(room => room.roomId == roomId);
+        io.to(roomId).emit('members', members);
+    })
+
+    socket.on('disconnect', () => {
+        const room = rooms.find(room => room.id == socket.id)
+        const index = rooms.findIndex(room => room.id == socket.id)
+        if (index !== -1) rooms.splice(index, 1)
+        if (room) {
+            io.to(room.roomId).emit('message', `Bot :${room.name}が退出しました。`)
+            socket.to(room.roomId).broadcast.emit('user-disconnected', room.peerId)
+            const members = rooms.filter(rm => rm.roomId == room.roomId);
+            io.to(room.roomId).emit('members', members);
+        }
+    });
+});
---
+++ public/app.js
@@ -0,0 +1,141 @@
+const socket = io({
+    autoConnect: false,
+});
+const videos = [];
+
+const app = Vue.createApp({
+    data: function () {
+        return {
+            message: "",
+            messages: [""],
+            name: '',
+            roomId: '',
+            members: [],
+            myPeer: new Peer(),
+            myVideo: '',
+            onVideo: true,
+            onMute: false,
+            myStream: '',
+        }
+    },
+    created() {
+    },
+    mounted() {
+        socket.on('message', (msg) => {
+            this.messages.push(msg)
+        });
+        socket.on('members', (members) => {
+            this.members = members;
+        });
+        this.setVideo()
+    },
+    methods: {
+        sendMessage() {
+            socket.emit('message', this.message)
+            this.message = ''
+        },
+        joinRoom(roomId) {
+            this.roomId = roomId;
+            this.myPeer = new Peer(undefined, {
+                host: '/',
+                port: 3000,
+                path: '/peerjs',
+                debug: 3
+            });
+            socket.open();
+            this.myPeer.on('open', peerId => {
+                socket.emit('join-room', this.roomId, this.name, peerId);
+            })
+        },
+        leaveRoom() {
+            this.roomId = "";
+            this.name = "";
+            this.messages = [];
+            this.members = [];
+            this.myPeer.destroy();
+            socket.close();
+        },
+        startVideo() {
+            this.onVideo = true;
+            const tracks = this.myStream.getVideoTracks();
+            tracks[0].enabled = true;
+        },
+        stopVideo() {
+            this.onVideo = false;
+            const tracks = this.myStream.getVideoTracks();
+            tracks[0].enabled = false;
+        },
+        startAudio() {
+            this.onMute = false;
+            const tracks = this.myStream.getVideoTracks();
+            tracks[0].mute = false;
+        },
+        stopAudio() {
+            this.onMute = true;
+            const tracks = this.myStream.getVideoTracks();
+            tracks[0].mute = true;
+        },
+        setVideo() {
+            this.myVideo = document.createElement('video')
+            this.myVideo.muted = true
+
+            navigator.mediaDevices.getUserMedia({
+                video: true,
+                audio: false,
+            }).then(stream => {
+                this.myStream = stream;
+                this.myVideo.srcObject = this.myStream;
+                this.myVideo.play()
+                this.$refs.video.append(this.myVideo)
+
+                this.myPeer.on('call', call => {
+                    const video = document.createElement('video')
+                    videos.push({
+                        video: video,
+                        peerId: call.peer,
+                    });
+                    call.answer(stream);
+
+                    call.on('stream', stream => {
+                        video.srcObject = stream;
+                        video.play()
+                        this.$refs.video.append(video)
+                    });
+
+                    call.on('close', () => {
+                        console.log('answerしたブラウザが退出')
+                        video.remove()
+                    })
+                });
+
+                socket.on('user-connected', (peerId) => {
+                    const call = this.myPeer.call(peerId, stream)
+                    const video = document.createElement('video')
+                    videos.push({
+                        video: video,
+                        peerId: call.peer,
+                    });
+                    call.on('stream', stream => {
+                        video.srcObject = stream;
+                        video.play()
+                        this.$refs.video.append(video)
+                    });
+                    //callした方が退室する際に実行
+                    call.on('close', () => {
+                        console.log('callしたブラウザで退出')
+                        video.remove()
+                    })
+                });
+
+                socket.on('user-disconnected', (peerId) => {
+                    const video = videos.find(video => video.peerId == peerId)
+                    if (video) video.video.remove();
+                })
+
+            }).catch(e => {
+                console.log(e)
+            })
+        }
+    }
+}).mount('#app')
+
